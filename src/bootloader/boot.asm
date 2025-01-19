75d4075b0e5fe20537d7887cc08b8f7e38289bc6
Marike <marikesmith90@gmail.com>
2025-01-19 15:22:20 +0200

    Enhance bootloader functionality and update README. Added interrupt handling and string printing in boot.asm; clarified boot command in README.md.

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -2,19 +2,41 @@ bits 16     ;16-bit mode
 org 0x7c00  ;bios loads the bootloader at 0x7c00
 
 start:
-    jmp 0x0000:main
+    jmp 0x0000:main    ; Far jump to clear CS:IP
 
-main:       ;set up the stack
-    mov ax, 0x0000
+main:
+    cli               ; Disable interrupts during setup
+    
+    ; Set up segment registers
+    xor ax, ax        ; Clear AX register (more explicit than mov ax, 0)
     mov ds, ax
     mov es, ax
     mov ss, ax
     mov sp, 0x7c00
+    
+    sti               ; Re-enable interrupts
 
     mov si, msg
     call print_string
+    
+    jmp $            ; Infinite loop after printing
+
+print_string:
+    push ax          ; Preserve registers
+    
+.loop:
+    lodsb
+    or al, al        ; Check if we reached end of string (0)
+    jz .done
+    mov ah, 0x0e     ; BIOS teletype output
+    int 0x10         ; Video interrupt
+    jmp .loop
+
+.done:
+    pop ax           ; Restore registers
+    ret
 
 msg: db 'Hello, World!', 0
 
-times 510 - ($ - $$) db 0	;fill the rest of sector with 0
-dw 0xAA55			;add boot signature at the end of bootloader    
+times 510 - ($ - $$) db 0    ; Fill the rest of sector with 0
+dw 0xAA55                    ; Boot signature    
